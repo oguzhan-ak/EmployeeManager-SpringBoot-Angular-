2aeaef7bb152bce1fa7e593ea45f3c2bb8e9a6c7
oğuzhan ak <oguzhan_ak@outlook.com.tr>
2023-11-27 10:13:59 +0300

    prime ng added

--- employeemanager-frontend/src/app/app.component.ts
+++ employeemanager-frontend/src/app/app.component.ts
@@ -1,106 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from './model/empoloyee';
-import { EmployeeService } from './service/employeeService';
-import { HttpErrorResponse } from '@angular/common/http';
-import { NgForm } from '@angular/forms';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  public employees: Employee[] = [];
-  public updateEmployee: Employee | undefined;
-  public deleteEmployee: Employee | undefined;
 
-  constructor(private employeeService: EmployeeService) {}
+    constructor(private primengConfig: PrimeNGConfig) { }
 
-  ngOnInit(): void {
-    this.getEmployees();
-  }
-
-  public getEmployees(): void {
-    this.employeeService.getEmployees().subscribe({
-      next: (employees: Employee[]) => {
-        this.employees = employees;
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-      },
-    });
-  }
-
-  public onAddEmployee(addForm: NgForm): void {
-    document.getElementById('add-employee-form')?.click();
-    this.employeeService.addEmployee(addForm.value).subscribe({
-      next: (response: Employee) => {
-        this.getEmployees();
-        addForm.reset();
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-        addForm.reset();
-      },
-    });
-  }
-
-  public onUpdateEmployee(employee: Employee): void {
-    this.employeeService.updateEmployee(employee).subscribe({
-      next: (response: Employee) => {
-        this.getEmployees();
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-      },
-    });
-  }
-
-  public onDeleteEmployee(employeeId: number): void {
-    this.employeeService.deleteEmployee(employeeId).subscribe({
-      next: (response: void) => {
-        this.getEmployees();
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.message);
-      },
-    });
-  }
-
-  public onSearchEmployee(key: string): void {
-    var results: Employee[] = [];
-    results = this.employees.filter((employee: Employee) => {
-      return (
-        employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
-        employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
-        employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
-        employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
-      );
-    });
-    this.employees = results;
-    if (!key) {
-      this.getEmployees();
+    ngOnInit() {
+        this.primengConfig.ripple = true;
     }
-  }
-
-  public onOpenModal(mode: string, employee?: Employee): void {
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.style.display = 'none';
-    button.setAttribute('data-toggle', 'modal');
-    if (mode === 'add') {
-      button.setAttribute('data-target', '#addEmployeeModal');
-    }
-    if (mode === 'update') {
-      this.updateEmployee = employee;
-      button.setAttribute('data-target', '#updateEmployeeModal');
-    }
-    if (mode === 'delete') {
-      this.deleteEmployee = employee;
-      button.setAttribute('data-target', '#deleteEmployeeModal');
-    }
-    var mainContainer = document.getElementById('main-container');
-    mainContainer?.appendChild(button);
-    button.click();
-  }
 }
